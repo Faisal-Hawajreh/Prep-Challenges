08270f419cb171dad998fd17ecbc6c68156951e0
Faisal Al-Hawajreh <faisal.alhawajreh@gmail.com>
2022-02-16 00:10:37 +0200

    don't pass challenge 18 yet

---
+++ code-challenges/test18.js
@@ -0,0 +1,17 @@
+// -------------------------------------------------------------------------------------------------------
+// Challenge 01:
+// Required:
+//
+// Given a string find the length of the middle word in the sentence
+//
+// Ex:
+// Input: "You can't handle the truth!"
+// Output: 6
+//
+// Input: "You're gonna need a bigger boat"
+// Output: 1
+//
+
+const wordLength = (str) => {
+    // write your code here
+}
